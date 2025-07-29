3a4eeedac1ef219c8a38bde86b0f7d2c9d76fc44
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-07-28 21:56:41 -0600

    Scaffold guest registation tests

---
+++ tests/Feature/RegisterPageTest.php
@@ -0,0 +1,8 @@
+<?php
+
+todo('allows guests to view the registration page');
+todo('allows guests to register with valid data');
+todo('does not allow registration with invalid data');
+todo('does not allow registration with an email that is already taken');
+todo('authenticates the user after successful registration');
+todo('redirects authenticated users away from the registration page');
