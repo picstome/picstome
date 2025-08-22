aded3e491095ed11b9b405499b4109c7714622a2
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-08-22 07:17:52 -0600

    Import Collection and clarify naturalSortBy macro for best practices and maintainability

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Team;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
 use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Cashier::useCustomerModel(Team::class);
+
+        Collection::macro('naturalSortBy', function ($attribute = 'name') {
+            /** @var \Illuminate\Support\Collection $this */
+            return $this->sort(function ($a, $b) use ($attribute) {
+                return strnatcmp($a->$attribute, $b->$attribute);
+            })->values();
+        });
     }
 }
