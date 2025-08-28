d6cd250f670439b54b3abcbbf042c06d9e316286
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-08-27 19:07:14 -0600

    Enable Spanish localization for selection limit notification

--- app/Notifications/SelectionLimitReached.php
+++ app/Notifications/SelectionLimitReached.php
@@ -24,13 +24,13 @@ class SelectionLimitReached extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject('Selection Limit Reached for Gallery: ' . $this->gallery->name)
-            ->greeting('Hello ' . $notifiable->name . ',')
-            ->line('Your customer has reached the selection limit of ' . $this->gallery->share_selection_limit . ' photos for the gallery "' . $this->gallery->name . '".')
-            ->line('You can now start working on the selected photos.')
-            ->line('Please note that the customer may have changed pictures in the meantime, so be sure to confirm with the customer before starting.')
-            ->action('View Gallery', route('galleries.show', ['gallery' => $this->gallery]))
-            ->salutation('Best regards, Picstome Team');
+            ->subject(__('Selection Limit Reached for Gallery: ') . $this->gallery->name)
+            ->greeting(__('Hello ') . $notifiable->name . ',')
+            ->line(__('Your customer has reached the selection limit of ') . $this->gallery->share_selection_limit . __(' photos for the gallery "') . $this->gallery->name . __('".'))
+            ->line(__('You can now start working on the selected photos.'))
+            ->line(__('Please note that the customer may have changed pictures in the meantime, so be sure to confirm with the customer before starting.'))
+            ->action(__('View Gallery'), route('galleries.show', ['gallery' => $this->gallery]))
+            ->salutation(__('Best regards, Picstome Team'));
     }
 
     public function toArray(object $notifiable): array
