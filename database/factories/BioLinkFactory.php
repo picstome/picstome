7b301e35dca0e26045e25c3e544e077db2645078
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-03 19:33:54 -0600

    Refactor BioLinkFactory to remove deprecated model property

--- database/factories/BioLinkFactory.php
+++ database/factories/BioLinkFactory.php
@@ -12,13 +12,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class BioLinkFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = BioLink::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
