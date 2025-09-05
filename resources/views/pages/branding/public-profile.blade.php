093b6f850ed0f580a49c7012069921e176d946b3
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-05 11:45:48 -0600

    Improve social links form organization with fieldset grouping

--- resources/views/pages/branding/public-profile.blade.php
+++ resources/views/pages/branding/public-profile.blade.php
@@ -77,71 +77,71 @@ new class extends Component
                                 <flux:error name="form.bio" />
                             </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>Instagram</flux:input.group.prefix>
-                                    <flux:input wire:model="form.instagram" placeholder="username" />
-                                </flux:input.group>
-                                <flux:error name="form.instagram" />
-                            </flux:field>
+                            <flux:fieldset>
+                                <flux:legend>{{ __('Social Links') }}</flux:legend>
+                                <flux:description>{{ __('Add your social media profiles and website to your public profile.') }}</flux:description>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>YouTube</flux:input.group.prefix>
-                                    <flux:input wire:model="form.youtube" placeholder="channel/UC123 or @username" />
-                                </flux:input.group>
-                                <flux:error name="form.youtube" />
-                            </flux:field>
+                                <div class="space-y-4">
+                                    <flux:field>
+                                        <flux:label>{{ __('Instagram') }}</flux:label>
+                                        <flux:input wire:model="form.instagram" placeholder="username" />
+                                        <flux:description>{{ __('Enter your Instagram username (without @)') }}</flux:description>
+                                        <flux:error name="form.instagram" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>Facebook</flux:input.group.prefix>
-                                    <flux:input wire:model="form.facebook" placeholder="username" />
-                                </flux:input.group>
-                                <flux:error name="form.facebook" />
-                            </flux:field>
+                                    <flux:field>
+                                        <flux:label>{{ __('YouTube') }}</flux:label>
+                                        <flux:input wire:model="form.youtube" placeholder="channel/UC123 or @username" />
+                                        <flux:description>{{ __('Enter your YouTube channel URL path or @username') }}</flux:description>
+                                        <flux:error name="form.youtube" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>X</flux:input.group.prefix>
-                                    <flux:input wire:model="form.x" placeholder="username" />
-                                </flux:input.group>
-                                <flux:error name="form.x" />
-                            </flux:field>
+                                    <flux:field>
+                                        <flux:label>{{ __('Facebook') }}</flux:label>
+                                        <flux:input wire:model="form.facebook" placeholder="username" />
+                                        <flux:description>{{ __('Enter your Facebook username or page name') }}</flux:description>
+                                        <flux:error name="form.facebook" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>TikTok</flux:input.group.prefix>
-                                    <flux:input wire:model="form.tiktok" placeholder="username" />
-                                </flux:input.group>
-                                <flux:error name="form.tiktok" />
-                            </flux:field>
+                                    <flux:field>
+                                        <flux:label>{{ __('X (Twitter)') }}</flux:label>
+                                        <flux:input wire:model="form.x" placeholder="username" />
+                                        <flux:description>{{ __('Enter your X (Twitter) username (without @)') }}</flux:description>
+                                        <flux:error name="form.x" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>Twitch</flux:input.group.prefix>
-                                    <flux:input wire:model="form.twitch" placeholder="username" />
-                                </flux:input.group>
-                                <flux:error name="form.twitch" />
-                            </flux:field>
+                                    <flux:field>
+                                        <flux:label>{{ __('TikTok') }}</flux:label>
+                                        <flux:input wire:model="form.tiktok" placeholder="username" />
+                                        <flux:description>{{ __('Enter your TikTok username (without @)') }}</flux:description>
+                                        <flux:error name="form.tiktok" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:input.group>
-                                    <flux:input.group.prefix>Website</flux:input.group.prefix>
-                                    <flux:input wire:model="form.website" placeholder="https://example.com" />
-                                </flux:input.group>
-                                <flux:error name="form.website" />
-                            </flux:field>
+                                    <flux:field>
+                                        <flux:label>{{ __('Twitch') }}</flux:label>
+                                        <flux:input wire:model="form.twitch" placeholder="username" />
+                                        <flux:description>{{ __('Enter your Twitch username') }}</flux:description>
+                                        <flux:error name="form.twitch" />
+                                    </flux:field>
 
-                            <flux:field>
-                                <flux:label>{{ __('Other') }}</flux:label>
-                                <div class="flex gap-2">
-                                    <flux:input wire:model="form.other.label" placeholder="Label" class="flex-1" />
-                                    <flux:input wire:model="form.other.url" placeholder="https://example.com" class="flex-1" />
+                                    <flux:field>
+                                        <flux:label>{{ __('Website') }}</flux:label>
+                                        <flux:input wire:model="form.website" placeholder="https://example.com" />
+                                        <flux:description>{{ __('Enter your website URL') }}</flux:description>
+                                        <flux:error name="form.website" />
+                                    </flux:field>
+
+                                    <flux:field>
+                                        <flux:label>{{ __('Other') }}</flux:label>
+                                        <div class="flex gap-2">
+                                            <flux:input wire:model="form.other.label" placeholder="Label" class="flex-1" />
+                                            <flux:input wire:model="form.other.url" placeholder="https://example.com" class="flex-1" />
+                                        </div>
+                                        <flux:error name="form.other.label" />
+                                        <flux:error name="form.other.url" />
+                                    </flux:field>
                                 </div>
-                                <flux:error name="form.other.label" />
-                                <flux:error name="form.other.url" />
-                            </flux:field>
+                            </flux:fieldset>
 
                             <flux:button type="submit" variant="primary">{{ __('Save') }}</flux:button>
                         </form>
