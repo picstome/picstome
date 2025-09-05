49e9b9c70af56ffcbe5400a6560b71e0f54cd38d
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-05 12:48:44 -0600

    Update show.blade.php

--- resources/views/handle/show.blade.php
+++ resources/views/handle/show.blade.php
@@ -8,7 +8,7 @@
         <div class="mx-auto w-full max-w-md text-center">
             <div class="space-y-4">
                 @if($team->brand_logo_icon_url)
-                    <flux:avatar size="xl" :src="$team->brand_logo_icon_url" class="mx-auto [:where(&)]:size-32" />
+                    <flux:avatar size="lg" size="xl" :src="$team->brand_logo_icon_url" class="mx-auto [:where(&)]:size-32" />
                 @endif
 
                 <flux:heading size="xl">{{ $team->name }}</flux:heading>
@@ -42,37 +42,37 @@
                      <div class="flex flex-wrap justify-center gap-4">
                          @if($team->instagram_url)
                              <a href="{{ $team->instagram_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://s.magecdn.com/social/tc-instagram.svg" />
+                                 <flux:avatar size="lg" circle src="https://s.magecdn.com/social/tc-instagram.svg" />
                              </a>
                          @endif
 
                          @if($team->youtube_url)
                              <a href="{{ $team->youtube_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://s.magecdn.com/social/tc-youtube.svg" />
+                                 <flux:avatar size="lg" circle src="https://s.magecdn.com/social/tc-youtube.svg" />
                              </a>
                          @endif
 
                          @if($team->facebook_url)
                              <a href="{{ $team->facebook_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://s.magecdn.com/social/tc-facebook.svg" />
+                                 <flux:avatar size="lg" circle src="https://s.magecdn.com/social/tc-facebook.svg" />
                              </a>
                          @endif
 
                          @if($team->x_url)
                              <a href="{{ $team->x_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://s.magecdn.com/social/tc-x.svg" />
+                                 <flux:avatar size="lg" circle src="https://s.magecdn.com/social/tc-x.svg" />
                              </a>
                          @endif
 
                          @if($team->tiktok_url)
                              <a href="{{ $team->tiktok_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://s.magecdn.com/social/tc-tiktok.svg" />
+                                 <flux:avatar size="lg" circle src="https://s.magecdn.com/social/tc-tiktok.svg" />
                              </a>
                          @endif
 
                          @if($team->twitch_url)
                              <a href="{{ $team->twitch_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle>
+                                 <flux:avatar size="lg" circle>
                                      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                                          <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286H13.714L22.286 10.857V0H6zm14.571 10.857l-3.429 3.429H13.714l-3 3v-3H6.857V1.714H20.57v9.143z"/>
                                      </svg>
@@ -82,13 +82,13 @@
 
                          @if($team->website_url)
                              <a href="{{ $team->website_url }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://unavatar.io/{{ parse_url($team->website_url, PHP_URL_HOST) }}" />
+                                 <flux:avatar size="lg" circle src="https://unavatar.io/{{ parse_url($team->website_url, PHP_URL_HOST) }}" />
                              </a>
                          @endif
 
                          @if($team->other_social_links)
                              <a href="{{ $team->other_social_links['url'] }}" target="_blank" rel="noopener noreferrer nofollow">
-                                 <flux:avatar circle src="https://unavatar.io/{{ parse_url($team->other_social_links['url'], PHP_URL_HOST) }}" />
+                                 <flux:avatar size="lg" circle src="https://unavatar.io/{{ parse_url($team->other_social_links['url'], PHP_URL_HOST) }}" />
                              </a>
                          @endif
                      </div>
