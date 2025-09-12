b5ed3db77a39ba91ee2837be75d88c6279774d61
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-12 10:01:11 -0600

    tui: add product checkout success page blade view

---
+++ resources/views/pages/product-checkout-success.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use function Laravel\Folio\middleware;
+use function Laravel\Folio\name;
+
+name('product-checkout-success');
+
+middleware(['auth', 'verified']);
+
+?>
+
+<div>
+    //
+</div>
