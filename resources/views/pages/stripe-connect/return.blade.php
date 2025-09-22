22d94198d7db9906102e556f666b3245e6bdc75a
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-09-22 14:23:20 -0600

    ui: refactor Stripe Connect return page to show onboarding status callout
    - Uses FluxUI callout to indicate onboarding completion or prompt user to finish onboarding
    - Adds onboarding link if not complete

--- resources/views/pages/stripe-connect/return.blade.php
+++ resources/views/pages/stripe-connect/return.blade.php
@@ -4,26 +4,48 @@ use Facades\App\Services\StripeConnectService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Livewire\Volt\Component;
 
 use function Laravel\Folio\name;
 use function Laravel\Folio\render;
 
 name('stripe.connect.return');
 
-render(function (Request $request, View $view) {
-    $team = Auth::user()->currentTeam;
+new class extends Component {
+    public $onboardingUrl;
+    public $onboardingComplete;
 
-    $onboardingComplete = StripeConnectService::isOnboardingComplete($team);
+    public function mount()
+    {
+        $team = Auth::user()->currentTeam;
 
-    $statusMessage = $onboardingComplete
-        ? 'Stripe onboarding complete! You can now receive payouts.'
-        : 'Stripe onboarding incomplete. Please finish setup to receive payouts.';
+        $this->onboardingComplete = StripeConnectService::isOnboardingComplete($team);
 
-    return $view->with('status', $statusMessage);
-});
+        $this->onboardingUrl = StripeConnectService::createOnboardingLink($team);
+    }
+} ?>
 
-?>
-
-<div>
-    {{ $status }}
-</div>
+<x-app-layout>
+    @volt('pages.stripe-connect.return')
+        <section class="mx-auto max-w-lg mt-10">
+            @if ($onboardingComplete)
+                <flux:callout variant="success" icon="check-circle">
+                    <flux:callout.heading>Stripe onboarding complete!</flux:callout.heading>
+                    <flux:callout.text>
+                        Your Stripe account is now connected. You can start accepting payments.
+                    </flux:callout.text>
+                </flux:callout>
+            @else
+                <flux:callout variant="warning" icon="exclamation-circle">
+                    <flux:callout.heading>Stripe onboarding not complete</flux:callout.heading>
+                    <flux:callout.text>
+                        You have not finished connecting your Stripe account. Please complete onboarding to accept payments.
+                    </flux:callout.text>
+                    <x-slot name="actions">
+                        <flux:button href="{{ $onboardingUrl }}" variant="primary" target="_blank">Continue Stripe Onboarding</flux:button>
+                    </x-slot>
+                </flux:callout>
+            @endif
+        </section>
+    @endvolt
+</x-app-layout>
