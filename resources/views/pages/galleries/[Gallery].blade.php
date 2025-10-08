6531f395f437a92da7d91f173566aa4b197b2c6f
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-08 15:21:34 -0600

    ui: preload gallery images in head for faster initial load

--- resources/views/pages/galleries/[Gallery].blade.php
+++ resources/views/pages/galleries/[Gallery].blade.php
@@ -734,5 +734,13 @@ new class extends Component
                 }));
             </script>
         @endscript
+
+        @push('head')
+            @foreach ($allPhotos as $photo)
+                <link rel="preload" as="image" href="{{ $photo->url }}">
+                <link rel="preload" as="image" href="{{ $photo->thumbnail_url }}">
+                <link rel="preload" as="image" href="{{ $photo->large_thumbnail_url }}">
+            @endforeach
+        @endpush
     @endvolt
 </x-app-layout>
