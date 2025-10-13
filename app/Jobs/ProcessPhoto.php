0e0ee9f4bd3e6b34ad44c1fd5f1f474dd90f7480
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-13 12:57:20 -0600

    Revert "core: robustly check for null/false from readStream in ProcessPhoto job constructor"
    This reverts commit c2d1cea1d55cf5d39c47017987c45d9aff497d8b.

--- app/Jobs/ProcessPhoto.php
+++ app/Jobs/ProcessPhoto.php
@@ -34,11 +34,8 @@ class ProcessPhoto implements ShouldQueue
         $readStream = Storage::disk('public')->readStream($this->photo->path);
         $writeStream = fopen($this->temporaryPhotoPath, 'w');
 
-        if (! $readStream) {
-            throw new Exception("Failed to open read stream for: {$this->photo->path}");
-        }
-        if ($writeStream === false) {
-            throw new Exception("Failed to open write stream for: {$this->temporaryPhotoPath}");
+        if ($readStream === false || $writeStream === false) {
+            throw new Exception('Failed to open streams for copying file.');
         }
 
         stream_copy_to_stream($readStream, $writeStream);
