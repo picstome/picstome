8c5eca9db8dd67d8543a94d007bd127140278264
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-10-13 10:18:25 -0600

    core: update StripeConnectService to use cashier.secret and remove unused test_secret config

--- app/Services/StripeConnectService.php
+++ app/Services/StripeConnectService.php
@@ -12,7 +12,7 @@ class StripeConnectService
 
     public function __construct()
     {
-        $this->apiKey = config('services.stripe.test_secret');
+        $this->apiKey = config('cashier.secret');
     }
 
     /**
