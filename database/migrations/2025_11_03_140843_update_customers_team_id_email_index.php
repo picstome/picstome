7c32f7fe050636a5feb08ef8f39cf28899686b75
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-03 08:11:06 -0600

    migration: add index update for customers team_id and email

---
+++ database/migrations/2025_11_03_140843_update_customers_team_id_email_index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropUnique('customers_team_id_email_unique');
+
+            $table->index(['team_id', 'email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            // Drop the regular index
+            $table->dropIndex(['team_id', 'email']);
+            // Restore the unique index
+            $table->unique(['team_id', 'email']);
+        });
+    }
+};
