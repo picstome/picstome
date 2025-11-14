183cb3c392030e5771283e7dd2160e1836d581d7
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-13 20:19:34 -0600

    refactor: localize GuestPhotoCommented notification and remove obsolete migration

--- app/Notifications/GuestPhotoCommented.php
+++ app/Notifications/GuestPhotoCommented.php
@@ -2,6 +2,8 @@
 
 namespace App\Notifications;
 
+use App\Models\Photo;
+use App\Models\PhotoComment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -13,15 +15,7 @@ class GuestPhotoCommented extends Notification
     /**
      * Create a new notification instance.
      */
-    public $photo;
-
-    public $comment;
-
-    public function __construct($photo, $comment)
-    {
-        $this->photo = $photo;
-        $this->comment = $comment;
-    }
+    public function __construct(public Photo $photo, public PhotoComment $comment) {}
 
     /**
      * Get the notification's delivery channels.
@@ -39,14 +33,14 @@ class GuestPhotoCommented extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject('New guest comment on your photo')
-            ->greeting('Hello!')
-            ->line('A guest has left a new comment on a photo in your gallery:')
-            ->line('Gallery: '.$this->photo->gallery->name)
-            ->line('Photo: '.$this->photo->name)
-            ->line('Comment: "'.$this->comment->comment.'"')
-            ->action('View Photo', url('/shares/'.$this->photo->gallery->ulid.'/photos/'.$this->photo->id))
-            ->line('You are receiving this because a guest commented on your shared gallery.');
+            ->subject(__('New guest comment on your photo'))
+            ->greeting(__('Hello!'))
+            ->line(__('A guest has left a new comment on a photo in your gallery:'))
+            ->line(__('Gallery: :name', ['name' => $this->photo->gallery->name]))
+            ->line(__('Photo: :name', ['name' => $this->photo->name]))
+            ->line(__('Comment: ":comment"', ['comment' => $this->comment->comment]))
+            ->action(__('View Photo'), url('/galleries/'.$this->photo->gallery->id.'/photos/'.$this->photo->id))
+            ->line(__('You are receiving this because a guest commented on your shared gallery.'));
     }
 
     /**
