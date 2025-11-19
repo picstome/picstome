d480f2d3a1af09e5d2e1e31b62a8eab31350a676
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-11-19 13:58:29 -0600

    core: schedule daily cleanup of livewire temporary files

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,4 @@ Artisan::command('inspire', function () {
 Schedule::command('galleries:delete-expired')->daily();
 Schedule::command('galleries:send-expiration-reminders')->dailyAt('09:00');
 Schedule::command('subscriptions:cleanup')->daily();
+Schedule::command('cleanup:livewire-tmp')->daily();
